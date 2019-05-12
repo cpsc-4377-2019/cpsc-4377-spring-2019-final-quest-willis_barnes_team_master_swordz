f337685feaacb5e418a47e1d75f4ad74bbe4cbdf
Mark Barnes <hail.nothing@gmail.com>
2019-05-11 21:01:11 -0500

    Added player hurt and death sounds

--- Advanced Game Programming/Advanced Game Programming/Source/ContactListener.cpp
+++ Advanced Game Programming/Advanced Game Programming/Source/ContactListener.cpp
@@ -4,6 +4,8 @@
 #include "Library.h"
 #include "BodyComponent.h"
 #include "HealthComponent.h"
+#include "AudioDevice.h"
+#include "Sound.h"
 
 void ContactListener::PreSolve(b2Contact* contact, const b2Manifold* oldManifold)
 {
@@ -26,18 +28,22 @@ void ContactListener::PreSolve(b2Contact* contact, const b2Manifold* oldManifold
 
  	if (objectA->getOwner()->getType() == ObjectType::bullet &&
 		objectB->getOwner()->getType() == ObjectType::enemy) {
+		objectB->getLibrary()->aDevice->playSound(objectB->getLibrary()->SFXLibrary["enemy_death"].get());
 		objectB->getOwner()->setIsDead(true);
 	}
 	if (objectB->getOwner()->getType() == ObjectType::bullet &&
 		objectA->getOwner()->getType() == ObjectType::enemy) {
+		objectA->getLibrary()->aDevice->playSound(objectA->getLibrary()->SFXLibrary["enemy_death"].get());
 		objectA->getOwner()->setIsDead(true);
 	}
 	if (objectA->getOwner()->getType() == ObjectType::arrow &&
 		objectB->getOwner()->getType() == ObjectType::player) {
+		objectB->getLibrary()->aDevice->playSound(objectB->getLibrary()->SFXLibrary["player_hurt"].get());
 		objectB->getOwner()->getComponent<HealthComponent>()->takeHit();
 	}
 	if (objectA->getOwner()->getType() == ObjectType::player &&
 		objectB->getOwner()->getType() == ObjectType::arrow) {
+		objectA->getLibrary()->aDevice->playSound(objectA->getLibrary()->SFXLibrary["player_hurt"].get());
 		objectA->getOwner()->getComponent<HealthComponent>()->takeHit();
 	}
 }
--- Advanced Game Programming/Advanced Game Programming/Source/HealthComponent.cpp
+++ Advanced Game Programming/Advanced Game Programming/Source/HealthComponent.cpp
@@ -2,6 +2,9 @@
 #include "Library.h"
 #include "Object.h"
 #include "Components.h"
+#include "AudioDevice.h"
+#include "Sound.h"
+#include "BodyComponent.h"
 
 HealthComponent::HealthComponent(Object* owner) : Components(owner) {
 	health = 100;
@@ -13,6 +16,7 @@ HealthComponent::~HealthComponent() {
 
 std::unique_ptr<Object> HealthComponent::update() {
 	if (health == 0) {
+		owner->getComponent<BodyComponent>()->getLibrary()->aDevice->playSound(owner->getComponent<BodyComponent>()->getLibrary()->SFXLibrary["player_death"].get());
 		owner->setIsDead(true);
 	}
 	return nullptr;
